a1eec0588d46f95eeeb809eae8ae35ce937c9905
mining4altcoins <nathanrafferty80@googlemail.com>
2018-02-10 17:46:50 +0000

    Name change
    Renaming of name to bitcoinwspectrum

--- qt/bitcoin.cpp
+++ qt/bitcoin.cpp
@@ -106,7 +106,7 @@ static std::string Translate(const char* psz)
 static void handleRunawayException(std::exception *e)
 {
     PrintExceptionContinue(e, "Runaway exception");
-    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. bitcoinwallstreet can no longer continue safely and will quit.") + QString("\n\n") + QString::fromStdString(strMiscWarning));
+    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. bitcoinwspectrum can no longer continue safely and will quit.") + QString("\n\n") + QString::fromStdString(strMiscWarning));
     exit(1);
 }
 
@@ -136,7 +136,7 @@ int main(int argc, char *argv[])
     {
         // This message can not be translated, as translation is not initialized yet
         // (which not yet possible because lang=XX can be overridden in bitcoin.conf in the data directory)
-        QMessageBox::critical(0, "bitcoinwallstreet",
+        QMessageBox::critical(0, "bitcoinwspectrum",
                               QString("Error: Specified data directory \"%1\" does not exist.").arg(QString::fromStdString(mapArgs["-datadir"])));
         return 1;
     }
@@ -144,12 +144,12 @@ int main(int argc, char *argv[])
 
     // Application identification (must be set before OptionsModel is initialized,
     // as it is used to locate QSettings)
-    app.setOrganizationName("bitcoinwallstreet");
+    app.setOrganizationName("bitcoinwspectrum");
     //XXX app.setOrganizationDomain("");
     if(GetBoolArg("-testnet")) // Separate UI settings for testnet
-        app.setApplicationName("bitcoinwallstreet-qt-testnet");
+        app.setApplicationName("bitcoinwspectrum-qt-testnet");
     else
-        app.setApplicationName("bitcoinwallstreet-qt");
+        app.setApplicationName("bitcoinwspectrum-qt");
 
     // ... then GUI settings:
     OptionsModel optionsModel;
